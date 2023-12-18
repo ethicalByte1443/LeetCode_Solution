fe6417a22dc3d93ceaec5c951a6e31fb19ae3e94
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2023-12-18 18:37:36 +0530

    text

--- 242 valid anagram/leet242.java
+++ 242 valid anagram/leet242.java
@@ -1,13 +1,4 @@
-// -----------Intuition-----------
-// you can follow a different approach that avoids creating two separate frequency maps. Instead, you can use a single array to represent the frequency of characters.
 
-// -----------Approach------------
-// Instead of creating two separate frequency maps, this version uses a single integer array (charCount) to represent the frequency of characters. This reduces the space complexity.
-// Complexity
-
-//     Time complexity:    This approach has a time complexity of O(n)
-
-//     Space complexity:   The space complexity of O(1)
 
 class leet242 {
     public boolean isAnagram(String s, String t) {
